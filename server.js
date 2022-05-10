dd8afa458725fd196431094849b10ccc9cf816fb
Praba <prama1303@gmail.com>
2022-05-10 23:33:45 +0200

    client ready to start

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send('Data');
+});
 
 const PORT = process.env.PORT || 6000;
 
